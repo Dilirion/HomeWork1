70ecb1175c8fd7f3629581a12d4de99b26527703
Dilirion <dilirion111@yandex.ru>
2015-11-07 19:28:48 +0700

    HomeWork1

---
+++ Domain/Cat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Cat
+    {
+        public Cat(int age)
+        {
+            _health = 5;
+            _age = age;
+            Color = new CatColor();
+        }
+
+        private string _name;
+        private readonly int _age;
+        private int _health;
+        public CatColor Color { set; get; }
+
+        public string CurrentColor => (_health >= 5) ? Color.HealthyColor : Color.SickColor;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (null == _name)
+                {
+                    _name = value;
+                }
+            } 
+        }
+
+        public int Age => _age;
+
+        public void Feed()
+        {
+            _health++;
+        }
+
+        public void Punish()
+        {
+            _health--;
+        }
+
+    }
+
+    public class CatColor
+    {
+        public string HealthyColor { set; get; } = "White";
+
+        public string SickColor { set; get; } = "Green";
+    }
+}
